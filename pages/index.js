be8da5d8a2edd6e6886fa01612f382ca6fc57b85
Luiz Henrique Guerra <lhguerra@gmail.com>
2018-03-23 17:55:49 -0300

    Added example with static files.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import styles from '../style.css'
 
+// import wally from '/static/wally.jpg'
+
 export default () => (
   <div className={ styles.main }>
     Welcome to next.js!
     <span className={ styles.truco }>truco</span>
+    <img src='/static/wally.jpg' />
   </div>
 )
